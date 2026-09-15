f7dc2c502e31d8a52d2b24327fec2721a8beee4f
Larimar4you <Zajanezlaja@gmail.com>
2026-09-16 00:40:55 +0200

    cars data types

---
+++ types/car.ts
@@ -0,0 +1,30 @@
+export interface CarLocation {
+  country: string;
+  city: string;
+  address: string;
+}
+
+export interface Car {
+  id: string;
+  year: number;
+  brand: string;
+  model: string;
+  type: string;
+  img: string;
+  description: string;
+  fuelConsumption: string;
+  engine: string;
+  features: string[];
+  rentalPrice: string;
+  rentalCompany: string;
+  location: CarLocation;
+  rentalConditions: string[];
+  mileage: number;
+}
+
+export interface CarsResponse {
+  cars: Car[];
+  totalCars: number;
+  page: number;
+  totalPages: number;
+}
